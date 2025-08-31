18dd816ed5b409aa37c3bf32799729cf47566021
Hoang Son <sondh2423@gmail.com>
2025-08-31 20:53:14 +0700

    Initial commitn

--- src/app/register/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import RegisterForm from '@/components/authen/register/RegisterForm';
-
-export default function RegisterPage() {
-  return <RegisterForm />;
-}
--- src/app/signin/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import SigninForm from '@/components/authen/signin/SigninForm';
-
-export default function SigninPage() {
-  return <SigninForm />;
-}
